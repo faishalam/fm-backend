42de88dec4cb15ad58a5bac7134649a4e93a821a
faishalam <faishalabdulmajid@gmail.com>
2026-03-03 20:09:02 +0700

    fix:sturcture folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [PrismaModule, UsersModule],
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaService } from '../../prisma/prisma.service';
 import { UserEntity } from './entities/user.entity';
 
 @Injectable()
